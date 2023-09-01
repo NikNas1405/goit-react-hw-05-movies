7076bf3eb4ac76164cbb1496f235629ba1c03b00
NikNas1405 <103371039+NikNas1405@users.noreply.github.com>
2023-09-01 18:10:06 +0000

    Deploying to gh-pages from @ NikNas1405/goit-react-hw-05-movies@96d52701616e6e3ceab228961c730e19c90e7c64 🚀

--- static/js/983.ddea531d.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[983],{983:function(e,i,n){n.r(i);var t=n(184);i.default=function(){return(0,t.jsx)("main",{children:(0,t.jsx)("h1",{children:"Hello"})})}}}]);
-//# sourceMappingURL=983.ddea531d.chunk.js.map
